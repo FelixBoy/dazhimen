8581ea46dd86e00367bbbcb259859e4c99462c4c
zhj-taishi <sdjtxdxyzhj@163.com>
2017-03-28 12:35:53 +0800

    修改IE下的一些js加载错误

--- web/javascript/MsgBox.js
+++ web/javascript/MsgBox.js
@@ -2,18 +2,18 @@
  * 消息管理工具类
  */
 var MsgBox = new Object();
-MsgBox.show= (function(msg){
-    $.messager.show({
-        title:'提示',
-        msg:msg,
-        showSpeed:100,
-        timeout:3000,
-        showType:'fade',
-        style:{
-            right:'',
-            top:document.body.scrollTop+document.documentElement.scrollTop,
-            bottom:'',
-        }
+MsgBox.show= (
+    function(msg){
+        $.messager.show({
+            title:'提示',
+            msg:msg,
+            showSpeed:100,
+            timeout:3000,
+            showType:'fade',
+            style:{
+                right:'',
+                top:document.body.scrollTop+document.documentElement.scrollTop,
+                bottom:''
+                    }
+            });
     });
-
-});
