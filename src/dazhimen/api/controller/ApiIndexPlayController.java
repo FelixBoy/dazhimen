129c259d50754d9822846e34ea5c578409b7c7f7
zhj-taishi <sdjtxdxyzhj@163.com>
2017-04-01 17:55:44 +0800

    添加了一些接口

---
+++ src/dazhimen/api/controller/ApiIndexPlayController.java
@@ -0,0 +1,16 @@
+package dazhimen.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/4/1.
+ */
+@Controller
+@RequestMapping("/api/indexplay")
+public class ApiIndexPlayController {
+    @RequestMapping("/getIndexPlay")
+    public void getIndexPlay(){
+
+    }
+}
